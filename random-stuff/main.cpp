49e9c08866e605f372d299a709c745dca6e61b4f
Tikhon Selifanov <tikhonselifanov@192.168.1.4>
2023-03-15 19:38:46 +0300

    Tower of Hanoi

--- random-stuff/main.cpp
+++ random-stuff/main.cpp
@@ -1,44 +1,76 @@
-#include <algorithm>
 #include <iostream>
-#include <string>
+#include <stdlib.h>
+#include <stdexcept>
 #include <vector>
-#include <set>
 
 using namespace std;
 
-template<typename T>
-void PrintRange(T begin, T end)
+class Tower
 {
-    bool isFirst = true;
-    for (auto x = begin; x != end; ++x)
+public:
+    // конструктор и метод SetDisks нужны, чтобы правильно создать башни
+    Tower(int disks_num)
     {
-        if (isFirst)
-        {
-            cout << *x;
-            isFirst = false;
-            continue;
-        }
-        cout << ' ' << *x;
+        FillTower(disks_num);
     }
-    cout << endl;
-}
 
-template<typename Cont, typename Elem>
-void FindAndPrint(Cont& cont, const Elem& el)
-{
-    auto iter = find_if(cont.begin(), cont.end(), [el](const Elem& elem)
+    int GetDisksNum() const
     {
-        return elem == el;
-    });
-    PrintRange(cont.begin(), iter);
-    PrintRange(iter, cont.end());
-}
+        return disks_.size();
+    }
 
-int main() {
-    set<int> test = {1, 1, 1, 2, 3, 4, 5, 5};
-    cout << "Test1"s << endl;
-    FindAndPrint(test, 3);
-    cout << "Test2"s << endl;
-    FindAndPrint(test, 0); // элемента 0 нет в контейнере
-    cout << "End of tests"s << endl;
+    void SetDisks(int disks_num)
+    {
+        FillTower(disks_num);
+    }
+
+    // добавляем диск на верх собственной башни
+    // обратите внимание на исключение, которое выбрасывается этим методом
+    void AddToTop(int disk)
+    {
+        int top_disk_num = disks_.size() - 1;
+        if (0 != disks_.size() && disk >= disks_[top_disk_num])
+        {
+            throw invalid_argument("Невозможно поместить большой диск на маленький");
+        }
+        else
+        {
+            disks_.push_back(disk);
+        }
+    }
+    // disks_num - количество перемещаемых дисков
+    // destination - конечная башня для перемещения
+    // buffer - башня, которую нужно использовать в качестве буфера для дисков
+    void MoveDisks(int disks_num, Tower& destination, Tower& buffer)
+    {
+        if (disks_num == 0)
+        {
+            return;
+        }
+        MoveDisks(disks_num - 1, buffer, destination);
+
+        destination.AddToTop(disks_.back());
+        disks_.pop_back();
+
+        buffer.MoveDisks(disks_num - 1, destination, *this);
+    }
+private:
+    vector<int> disks_;
+
+    // используем приватный метод FillTower, чтобы избежать дубликации кода
+    void FillTower(int disks_num)
+    {
+        for (int i = disks_num; i > 0; i--)
+        {
+            disks_.push_back(i);
+        }
+    }
+};
+
+void SolveHanoi(vector<Tower>& towers) {
+    int disks_num = towers[0].GetDisksNum();
+    // запускаем рекурсию
+    // просим переложить все диски на последнюю башню
+    // с использованием средней башни как буфера
+    towers[0].MoveDisks(disks_num, towers[2], towers[1]);
 }
