7565cad9b47f9bae24fc839ea20697b5af5412b5
Tikhon Selifanov <tikhonselifanov@192.168.1.4>
2023-03-23 14:28:00 +0300

    Requests via deque

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -9,6 +9,7 @@
 #include <numeric>
 #include <stdexcept>
 #include <unordered_map>
+#include <queue>
 
 using namespace std;
 
@@ -489,25 +490,82 @@ private:
 
 };
 
+class RequestQueue {
+public:
+    explicit RequestQueue(const SearchServer& search_server) : server_(search_server)
+    {
+        count_no_res_ = 0;
+    }
+    // сделаем "обёртки" для всех методов поиска, чтобы сохранять результаты для нашей статистики
+    template <typename DocumentPredicate>
+    vector<Document> AddFindRequest(const string& raw_query, DocumentPredicate document_predicate)
+    {
+        vector<Document> vec = server_.FindTopDocuments(raw_query, document_predicate);
+        if (vec.empty())
+        {
+            ++count_no_res_;
+        }
+        requests_.push_front({vec.empty()});
+
+        if (requests_.size() > min_in_day_)
+        {
+            if (requests_.back().is_empty)
+            {
+                --count_no_res_;
+            }
+            requests_.pop_back();
+        }
+
+        return vec;
+    }
+    vector<Document> AddFindRequest(const string& raw_query, DocumentStatus status)
+    {
+        return AddFindRequest(raw_query, [status](int id, DocumentStatus stat, int rating){ return stat == status;});
+    }
+    vector<Document> AddFindRequest(const string& raw_query)
+    {
+        return AddFindRequest(raw_query, DocumentStatus::ACTUAL);
+    }
+    int GetNoResultRequests() const
+    {
+        return count_no_res_;
+    }
+private:
+    struct QueryResult
+    {
+        bool is_empty;
+    };
+    const SearchServer& server_;
+    deque<QueryResult> requests_;
+    const static int min_in_day_ = 1440;
+    int count_no_res_;
+    // возможно, здесь вам понадобится что-то ещё
+};
+
 template <typename Container>
 auto Paginate(const Container& c, size_t page_size)
 {
     return Paginator(begin(c), end(c), page_size);
 }
-int main()
-{
-    SearchServer search_server("and with"s);
-    search_server.AddDocument(1, "funny pet and nasty rat"s, DocumentStatus::ACTUAL, {7, 2, 7});
-    search_server.AddDocument(2, "funny pet with curly hair"s, DocumentStatus::ACTUAL, {1, 2, 3});
-    search_server.AddDocument(3, "big cat nasty hair"s, DocumentStatus::ACTUAL, {1, 2, 8});
-    search_server.AddDocument(4, "big dog cat Vladislav"s, DocumentStatus::ACTUAL, {1, 3, 2});
-    search_server.AddDocument(5, "big dog hamster Borya"s, DocumentStatus::ACTUAL, {1, 1, 1});
-    const auto search_results = search_server.FindTopDocuments("curly dog"s);
-    int page_size = 2;
-    const auto pages = Paginate(search_results, page_size);
-    // Выводим найденные документы по страницам
-    for (auto page = pages.begin(); page != pages.end(); ++page) {
-        cout << *page << endl;
-        cout << "Page break"s << endl;
+
+int main() {
+    SearchServer search_server("and in at"s);
+    RequestQueue request_queue(search_server);
+    search_server.AddDocument(1, "curly cat curly tail"s, DocumentStatus::ACTUAL, {7, 2, 7});
+    search_server.AddDocument(2, "curly dog and fancy collar"s, DocumentStatus::ACTUAL, {1, 2, 3});
+    search_server.AddDocument(3, "big cat fancy collar "s, DocumentStatus::ACTUAL, {1, 2, 8});
+    search_server.AddDocument(4, "big dog sparrow Eugene"s, DocumentStatus::ACTUAL, {1, 3, 2});
+    search_server.AddDocument(5, "big dog sparrow Vasiliy"s, DocumentStatus::ACTUAL, {1, 1, 1});
+    // 1439 запросов с нулевым результатом
+    for (int i = 0; i < 1439; ++i) {
+        request_queue.AddFindRequest("empty request"s);
     }
+    // все еще 1439 запросов с нулевым результатом
+    request_queue.AddFindRequest("curly dog"s);
+    // новые сутки, первый запрос удален, 1438 запросов с нулевым результатом
+    request_queue.AddFindRequest("big collar"s);
+    // первый запрос удален, 1437 запросов с нулевым результатом
+    request_queue.AddFindRequest("sparrow"s);
+    cout << "Total empty requests: "s << request_queue.GetNoResultRequests() << endl;
+    return 0;
 }
