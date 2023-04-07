f42b1e02734d895c40e6ecbde9a4802992c4aaf8
Tikhon Selifanov <tikhonselifanov@192.168.1.4>
2023-04-07 23:45:24 +0300

    No_RAM

--- random-stuff/main.cpp
+++ random-stuff/main.cpp
@@ -1,79 +1,28 @@
 #include <iostream>
-#include <numeric>
-#include <sstream>
-#include <vector>
-#include <algorithm>
+#include <new>
+#include <string>
 
 using namespace std;
 
-template<typename T>
-void PrintRange(T begin, T end)
+int main()
 {
-    bool isFirst = true;
-    for (auto x = begin; x != end; ++x)
+    size_t n = 0;
+    try
     {
-        if (isFirst)
+        // Так создаётся бесконечный цикл.
+        for (;;)
         {
-            cout << *x;
-            isFirst = false;
-            continue;
-        }
-        cout << ' ' << *x;
-    }
-    cout << endl;
-}
-
-template<typename Iter>
-void Merge(Iter beg, Iter mid, Iter end)
-{
-    std::vector<typename Iter::value_type> temp;
-
-    Iter a;
-    Iter left = beg;
-    Iter right = mid;
-    while (left != mid && right != end)
-    {
-        if (*right < *left)
-        {
-            temp.emplace_back(*right++);
-        }
-        else
-        {
-            temp.emplace_back(*left++);
+            // Внимание! Эта программа ведёт себя некорректно в иллюстративных целях.
+            // Указатель на выделенную в куче строку никуда не сохраняется, что
+            // приводит к утечке памяти.
+            new string(100'000'000, ' ');
+            ++n;
         }
     }
-    temp.insert(temp.end(), left, mid);
-    temp.insert(temp.end(), right, end);
-
-    std::move(temp.begin(), temp.end(), beg);
-}
-
-template <typename RandomIt>
-void MergeSort(RandomIt range_begin, RandomIt range_end)
-{
-    int size = distance(range_begin, range_end);
-    auto mid = next(range_begin, size / 2);
-
-    if (size <= 1)
+    catch (const bad_alloc&)
     {
-        return;
+        // Рано или поздно доступная программе память закончится,
+        // и оператор new выбросит исключение bad_alloc
+        cout << "bad_alloc after "s << n << " allocations"s << endl;
     }
-
-    MergeSort(range_begin, mid);
-    MergeSort(mid, range_end);
-    Merge(range_begin, mid, range_end);
-}
-
-int main() {
-    vector<int> test_vector = {5, 2, 3, 1};
-    // iota             -> http://ru.cppreference.com/w/cpp/algorithm/iota
-    // Заполняет диапазон последовательно возрастающими значениями
-
-    // Выводим вектор до сортировки
-    PrintRange(test_vector.begin(), test_vector.end());
-    // Сортируем вектор с помощью сортировки слиянием
-    MergeSort(test_vector.begin(), test_vector.end());
-    // Выводим результат
-    PrintRange(test_vector.begin(), test_vector.end());
-    return 0;
 }
