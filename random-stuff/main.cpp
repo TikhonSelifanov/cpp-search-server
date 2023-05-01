0a1974e1a4e24318bb73878d907b6a137b86a947
Tikhon Selifanov <tikhonselifanov@192.168.1.4>
2023-05-01 17:47:06 +0300

    JosephusPermutation

--- random-stuff/main.cpp
+++ random-stuff/main.cpp
@@ -1,28 +1,98 @@
+#include <cassert>
 #include <iostream>
-#include <new>
-#include <string>
+#include <numeric>
+#include <vector>
+#include <list>
+#include <iostream>
+#include <utility>
+#include <iterator>
 
 using namespace std;
 
-int main()
+template <typename RandomIt>
+void MakeJosephusPermutation(RandomIt first, RandomIt last, uint32_t step_size)
 {
-    size_t n = 0;
-    try
+    list<typename RandomIt::value_type> pool(make_move_iterator(first), make_move_iterator(last));
+    size_t cur_pos = 0;
+    auto it = pool.begin();
+    while (!pool.empty())
     {
-        // Так создаётся бесконечный цикл.
-        for (;;)
+        it = pool.begin();
+        advance(it, cur_pos);
+        *(first++) = move(*it);
+        pool.erase(it);
+        if (pool.empty())
         {
-            // Внимание! Эта программа ведёт себя некорректно в иллюстративных целях.
-            // Указатель на выделенную в куче строку никуда не сохраняется, что
-            // приводит к утечке памяти.
-            new string(100'000'000, ' ');
-            ++n;
+            break;
         }
+        cur_pos = (cur_pos + step_size - 1) % pool.size();
     }
-    catch (const bad_alloc&)
+}
+
+vector<int> MakeTestVector() {
+    vector<int> numbers(10);
+    iota(begin(numbers), end(numbers), 0);
+    return numbers;
+}
+
+void TestIntVector() {
+    const vector<int> numbers = MakeTestVector();
     {
-        // Рано или поздно доступная программе память закончится,
-        // и оператор new выбросит исключение bad_alloc
-        cout << "bad_alloc after "s << n << " allocations"s << endl;
+        vector<int> numbers_copy = numbers;
+        MakeJosephusPermutation(begin(numbers_copy), end(numbers_copy), 1);
+        assert(numbers_copy == numbers);
     }
+    {
+        vector<int> numbers_copy = numbers;
+        MakeJosephusPermutation(begin(numbers_copy), end(numbers_copy), 3);
+        assert(numbers_copy == vector<int>({0, 3, 6, 9, 4, 8, 5, 2, 7, 1}));
+    }
+}
+
+// Это специальный тип, который поможет вам убедиться, что ваша реализация
+// функции MakeJosephusPermutation не выполняет копирование объектов.
+// Сейчас вы, возможно, не понимаете как он устроен, однако мы расскажем
+// об этом далее в нашем курсе
+
+struct NoncopyableInt {
+    int value;
+
+    NoncopyableInt(const NoncopyableInt&) = delete;
+    NoncopyableInt& operator=(const NoncopyableInt&) = delete;
+
+    NoncopyableInt(NoncopyableInt&&) = default;
+    NoncopyableInt& operator=(NoncopyableInt&&) = default;
+};
+
+bool operator==(const NoncopyableInt& lhs, const NoncopyableInt& rhs) {
+    return lhs.value == rhs.value;
+}
+
+ostream& operator<<(ostream& os, const NoncopyableInt& v) {
+    return os << v.value;
+}
+
+void TestAvoidsCopying() {
+    vector<NoncopyableInt> numbers;
+    numbers.push_back({1});
+    numbers.push_back({2});
+    numbers.push_back({3});
+    numbers.push_back({4});
+    numbers.push_back({5});
+
+    MakeJosephusPermutation(begin(numbers), end(numbers), 2);
+
+    vector<NoncopyableInt> expected;
+    expected.push_back({1});
+    expected.push_back({3});
+    expected.push_back({5});
+    expected.push_back({4});
+    expected.push_back({2});
+
+    assert(numbers == expected);
+}
+
+int main() {
+    TestIntVector();
+    TestAvoidsCopying();
 }
